8832b70327d9ce66b7a2ebcdd552991a9e54ecfe
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-06 11:59:23 +0700

    🆙src/app/page: usage navbar comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col w-full">
-      <img
-        src="/glowingskin-banner.png"
-        alt="banner"
-        className="w-full h-auto object-cover"
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex min-h-screen flex-col w-full lg:px-14 ">
+        <img
+          src="/glowingskin-banner.png"
+          alt="banner"
+          className="w-full h-auto object-cover"
+        />
+      </div>
+    </>
   );
 }
