701591d2e306c0637e0fd5875cfcd68ae8333a93
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-06 11:26:39 +0700

    💄src/app/page: display banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1>Home</h1>
+    <div className="flex min-h-screen flex-col w-full">
+      <img
+        src="/glowingskin-banner.png"
+        alt="banner"
+        className="w-full h-auto object-cover"
+      />
     </div>
   );
 }
