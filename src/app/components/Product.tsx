6e54346221b495786d62cfb4e3fb25bba3a36ae8
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-06 13:14:38 +0700

    ⭐create products comp (mobile size)

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,49 @@
+export default function Product() {
+  const products = [
+    {
+      src: "/produk-1.png",
+      title: "Product 1",
+      desc: "desc for product 1.",
+    },
+    {
+      src: "/produk-2.png",
+      title: "Product 2",
+      desc: "desc for product 2.",
+    },
+    {
+      src: "/produk-3.png",
+      title: "Product 3",
+      desc: "desc for product 3.",
+    },
+    {
+      src: "/produk-4.png",
+      title: "Product 4",
+      desc: "desc for product 4.",
+    },
+  ];
+
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-4">
+        {products.map((product, index) => (
+          <div
+            key={product.title}
+            className={`flex items-center gap-4 ${
+              index % 2 !== 0 ? "flex-row-reverse" : "flex-row"
+            }`}
+          >
+            <img
+              src={product.src}
+              alt={product.title}
+              className="w-32 sm:w-48 md:w-64 rounded-md"
+            />
+            <div>
+              <h2 className="text-[#644040] font-semibold">{product.title}</h2>
+              <p className="text-[#644040] font-light">{product.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
