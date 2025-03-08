3f791e3248b42bc185ea8ccc87556b59425fe5cd
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 09:21:19 +0700

    💄available: add link

--- src/app/components/Available.tsx
+++ src/app/components/Available.tsx
@@ -3,17 +3,26 @@ import AvailableCard from "./AvailableCard";
 
 const Available = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 lg:px-14">
       <h1 className="text-3xl font-bold text-[#644040] text-center">
         Order Now!
       </h1>
 
       <div className="grid grid-cols-1 gap-3 md:grid-cols-3 lg:flex lg:justify-between">
-        <AvailableCard eCommerceName="Tokopedia" />
-        <AvailableCard eCommerceName="Bukalapak" />
-        <AvailableCard eCommerceName="Shopee" />
-        <AvailableCard eCommerceName="Blibli" />
-        <AvailableCard eCommerceName="Lazada" />
+        <AvailableCard
+          eCommerceName="Tokopedia"
+          link="https://www.tokopedia.com/"
+        />
+        <AvailableCard
+          eCommerceName="Bukalapak"
+          link="https://www.bukalapak.com"
+        />
+        <AvailableCard
+          eCommerceName="Shopee"
+          link="https://www.shopee.co.id/"
+        />
+        <AvailableCard eCommerceName="Blibli" link="https://www.blibli.com" />
+        <AvailableCard eCommerceName="Lazada" link="https://www.lazada.co.id" />
       </div>
     </div>
   );
