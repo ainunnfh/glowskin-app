af825e81698844fd9aac1facc1427d8a86450161
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 08:59:40 +0700

    ⭐create address comp

---
+++ src/app/components/Address.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div className="flex flex-col gap-2 ">
+      <div>
+        <img src="logo.png" alt="logo" className="w-32" />
+      </div>
+
+      <h1 className="font-bold ml-5 text-[#644040]">Address</h1>
+
+      <div className="text-[#644040] ml-5">
+        <p>Jl. Arah Pemuda No 78</p>
+        <p>Jakarta</p>
+        <p>112233</p>
+        <p>012345678910</p>
+        <p>glowingskin.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Address;
