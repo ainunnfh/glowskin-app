2ba5393ca9629de1bd9ac99f19eab3a1e5de9584
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 09:22:05 +0700

    💄best product: change font color and size

--- src/app/components/BestProduct.tsx
+++ src/app/components/BestProduct.tsx
@@ -4,9 +4,7 @@ import BestProductCard from "./BestProductCard";
 const BestProduct = () => {
   return (
     <div className="flex flex-col justify-center items-center p-4 bg-[#ffebeb] gap-3">
-      <h1 className="text-2xl font-bold mb-3 bg-gradient-to-r from-[#d7a7aa] to-[#644040] text-transparent bg-clip-text">
-        Best Product
-      </h1>
+      <h1 className="text-3xl font-bold mb-3 text-[#644040]">Best Product</h1>
 
       <div className="flex flex-col gap-3 md:flex-row md:justify-around w-full">
         <BestProductCard image="best-product-1.png" title="Best Product 1" />
