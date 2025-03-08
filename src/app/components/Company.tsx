5ca878c0909ab7e6932ff48a43188bf509957e64
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 08:59:52 +0700

    ⭐create company comp

---
+++ src/app/components/Company.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Company = () => {
+  return (
+    <div className="p-2 ml-5 text-[#644040]">
+      <h1 className="font-bold">Company</h1>
+
+      <div>
+        <p>About</p>
+        <p>Contact us</p>
+        <p>Career</p>
+        <p>Gallery</p>
+        <p>Blog</p>
+        <p>Privacy</p>
+        <p>Terms and Conditions</p>
+      </div>
+    </div>
+  );
+};
+
+export default Company;
