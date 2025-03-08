7e7c17b95604ea96033cf3b3923aa702358d2ddb
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 09:00:07 +0700

    ⭐create product category comp

---
+++ src/app/components/ProductCategories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProductCategories = () => {
+  return (
+    <div className="p-2 text-[#644040] ml-5">
+      <h1 className="font-bold">Product</h1>
+
+      <div>
+        <p>Promo</p>
+        <p>New Arrival</p>
+        <p>Best Seller</p>
+        <p>Normal Skin</p>
+        <p>Oily Skin</p>
+        <p>Dry Skin</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCategories;
