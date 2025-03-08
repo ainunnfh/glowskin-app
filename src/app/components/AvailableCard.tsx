078118a018f79aa04e1cb12f6405fa0683c5d324
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-08 09:18:54 +0700

    💄available card: add href

--- src/app/components/AvailableCard.tsx
+++ src/app/components/AvailableCard.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 interface IAvailableCardProps {
   eCommerceName: string;
+  link: string;
 }
-const AvailableCard: React.FC<IAvailableCardProps> = ({ eCommerceName }) => {
+const AvailableCard: React.FC<IAvailableCardProps> = ({
+  eCommerceName,
+  link,
+}) => {
   return (
     <div className="flex flex-col items-center p-3">
       <p className="bg-[#644040] text-white p-2 rounded-md w-40 text-center">
-        {eCommerceName}
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {eCommerceName}
+        </a>
       </p>
     </div>
   );
